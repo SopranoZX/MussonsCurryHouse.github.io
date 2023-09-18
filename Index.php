2a59b008184c7cf87d38439abff455a52c661f0e
Georgii <qubby2003@yandex.ru>
2023-09-18 20:52:26 +0300

    Initial commit

---
+++ Index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <link rel="stylesheet" href="css/style.css"> 
+
+
+</head>
+<body>
+
+    <div class="main-desktop-image">
+
+        <div class="main-title">
+            <h1>
+                Masons Curry House
+            </h1>
+        </div>
+        <div id="menu-list">
+            
+                <a>
+                    Menu
+                </a>
+            
+            
+                <a>
+                    Reservation
+                </a>
+            
+            
+                <a>
+                    About us
+                </a>
+            
+            
+                <a>
+                    Gift Card
+                </a>
+            
+        </div>
+    </div>
+
+    <div class="content-info">
+
+        <div class="content-info-title">
+                <a>
+                    Address 
+                    <br><br>
+                <label>809 Rnadal Street,   
+                            <br> 
+                    West Cork</label>
+                </a>
+                <a>
+                    Hours 
+                            <br><br>
+                <label>Sunday - Thuresday
+                            <br>
+                    4:30pm -  10:00pm
+                            <br><br>
+                    Friday - Saturday
+                            <br>
+                    4:30pm -  11:00pm</label>
+                </a>
+                <a>
+                    Contact 
+                            <br><br>
+                <label>(021) 489 9570
+                            <br>
+                    MasonCurry@gmail.com
+                </label>
+                </a>
+        </div>
+                <br>
+
+    </div>
+
+    <div class="background-photo-bar">
+        <h2>
+            Try Our Award Winning Lager
+        </h2>
+    </div>
+
+    <div class="content-about-us">
+
+        <img src="/images/photo-about-us.jpg">
+        <p>
+        <span>
+            About us
+        </span>
+        <br> <br>
+        As one of the first restaurants in West Cork to bring exciting indian flavours to Ireland,
+        James Masons Curry House was founded in 2010 with a goal of serving bold, 
+        global flavours to our local community (and visitors!). Since then, our desire to treat 
+        guests like family has remained the same (as have our naan bread) – but our seasonal 
+        flavours, cocktails, wine and locally-rooted beer are ever-changing.
+        </p>
+
+    </div>
+
+    <div class="footer-desktop-photo">
+        
+    </div>
+
+    <div class="footer-desktop">
+
+        <p>
+            Join the Family and our Mail List
+        </p>
+
+        <input type="button" value="Sign up">
+        
+    </div>
+
+    <br>
+
+    <div class="footer-links">
+
+        <a>CABRA</a> <a>BOKA RESTAURANT GROUP</a> <a>LITTLE GOAT DINER</a> <a>DUCK DUCK GOAT</a> <a>BAOBING</a>
+
+        <br>
+             © 2023 Boka Restaurant Group, LLC. All Rights Reserved | <a>Accessibility</a> | <a>Privacy <br> Policy</a> | <a>Site Map</a> | <a>Employment</a> | <a>Press Inquiries</a>
+
+    </div>
+</body>
+</html>
